f797bd2c0ebe3fa480de5d00d1544dad35f18244
Lada Vorontsova <ladar1993@gmail.com>
2020-06-12 19:59:15 +0300

    Залила проект

---
+++ app/src/main/java/com/example/ti_pidor/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ti_pidor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.text.*;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       Button btn = (Button) findViewById(R.id.button2);
+       final TextView textView = (TextView) findViewById(R.id.textView);
+
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               double rnd = Math.random();
+               if(rnd>0.5){
+                   textView.setText("Ti pidor");
+               }
+               else{
+                   textView.setText("Ti utochka");
+               }
+           }
+       });
+
+
+
+
+        //Button button2 = new Button();
+    }
+}
